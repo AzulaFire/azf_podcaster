e3ccd07c25ea98a3fa4dbea257b8fb4e459c1dcc
AzulaFire <jhornjr@gmail.com>
2024-07-18 14:40:33 +0900

    working commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: 'https://expert-cardinal-12.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
+
+export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lovely-flamingo-139.convex.cloud',
+      },
+      {
+        protocol: 'https',
+        hostname: 'veracious-greyhound-608.convex.cloud',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
